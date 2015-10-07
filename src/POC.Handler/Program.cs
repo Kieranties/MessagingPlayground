43b3d6c2283c64de2fc5944468e45c66ac128280
Kieranties <kmn@sitecore.net>
2015-10-07 16:33:12 +0100

    Trivial message handler implemented

--- src/POC.Handler/Program.cs
+++ src/POC.Handler/Program.cs
@@ -1,14 +1,63 @@
-﻿using System;
+﻿using Microsoft.Framework.Logging;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Messaging;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using POC.Messages.Commands;
+using POC.Integration.Workflows;
+using Microsoft.Framework.DependencyInjection;
 
 namespace POC.Handler
 {
     public class Program
     {
+        private readonly ILogger<Program> _logger;
+        private readonly IServiceProvider _services;
+
+        public Program()
+        {
+            _services = BuildServiceProvider();
+            _logger = _services.GetRequiredService<ILogger<Program>>();
+        }
+
         public void Main(string[] args)
         {
+            using(var queue = new MessageQueue(".\\private$\\poc.messagequeue.unsubscribe"))
+            {
+                while (true)
+                {
+                    _logger.LogInformation("Listening");
+
+                    var message = queue.Receive();
+                    var bodyReader = new StreamReader(message.BodyStream);
+                    var jsonBody = bodyReader.ReadToEnd();
+                    var unsubscribeMessage = JsonConvert.DeserializeObject<UnsubscribeCommand>(jsonBody);
+                    var workflow = new UnsubscribeWorkflow(unsubscribeMessage.EmailAddress);
+                    _logger.LogDebug($"Starting unsubscribe for {unsubscribeMessage.EmailAddress} ");
+                    workflow.Run();
+                    _logger.LogDebug($"Unsubscribe complete for {unsubscribeMessage.EmailAddress} ");
+                }
+            }
+        }
+        
+        private IServiceProvider BuildServiceProvider()
+        {
+            var services = new ServiceCollection().AddLogging();
+            var provider = services.BuildServiceProvider();
+
+            Configure(provider);
+
+            return provider;
+        }
+
+        private void Configure(IServiceProvider services)
+        {
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+            loggerFactory.MinimumLevel = LogLevel.Debug;
+            loggerFactory.AddConsole(loggerFactory.MinimumLevel);
         }
     }
 }
--- src/POC/Controllers/UnsubscribeController.cs
+++ src/POC/Controllers/UnsubscribeController.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNet.Mvc;
 using POC.Integration.Workflows;
 using POC.Messages.Commands;
 using System.Messaging;
+using Newtonsoft.Json;
+using System.Text;
+using System.IO;
 
 namespace POC.Controllers
 {
@@ -18,8 +21,27 @@ namespace POC.Controllers
         [HttpPost]
         public IActionResult Submit(string emailAddress)
         {
-            var workflow = new UnsubscribeWorkflow(emailAddress);
-            workflow.Run();
+            //var workflow = new UnsubscribeWorkflow(emailAddress);
+            //workflow.Run();
+
+            var unsubscribeCommand = new UnsubscribeCommand
+            {
+                EmailAddress = emailAddress
+            };
+
+            using(var queue = new MessageQueue(".\\private$\\poc.messagequeue.unsubscribe"))
+            {
+                // serialize using default xml serialization
+                // var message = new Message(unsubscribeCommand); //
+
+                // serailize to json and write raw bytes
+                var message = new Message();
+                var json = JsonConvert.SerializeObject(unsubscribeCommand);
+                message.BodyStream = new MemoryStream(Encoding.Default.GetBytes(json));
+                queue.Send(message);
+
+                // json raw bytes cleraly reduces size of message
+            }
 
             return View("Confirmation");
         }
