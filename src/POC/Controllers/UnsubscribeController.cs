5b106c40f0556343118877abac3afb0700b36a58
Kieranties <kmn@sitecore.net>
2015-10-07 15:58:39 +0100

    Working stub project - No queue usage

--- src/POC/Controllers/UnsubscribeController.cs
+++ src/POC/Controllers/UnsubscribeController.cs
@@ -1,20 +1,26 @@
 using System.Linq;
 using Microsoft.AspNet.Mvc;
 using POC.Integration.Workflows;
+using POC.Messages.Commands;
+using System.Messaging;
 
 namespace POC.Controllers
 {
+    [Route("/[controller]")]
     public class UnsubscribeController : Controller
     {
+        [HttpGet]        
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
         public IActionResult Submit(string emailAddress)
         {
             var workflow = new UnsubscribeWorkflow(emailAddress);
             workflow.Run();
+
             return View("Confirmation");
         }
     }
