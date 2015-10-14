baa9d703272740d583020ce34b636c787ab8b6cf
Kieranties <kmn@sitecore.net>
2015-10-14 21:52:56 +0100

    Added startup script

---
+++ startup.ps1
@@ -0,0 +1,14 @@
+﻿param(
+	[string]$queueType = 'msmq',
+	[string]$siteCommand = 'web',
+	[string[]]$handlerCommands = @('user', 'unsubscribe', 'legacy', 'crm', 'fulfilment'),
+	[string]$sitePath = (Resolve-Path "$PSScriptRoot\src\POC"),
+	[string]$handlerPath = (Resolve-Path "$PSScriptRoot\src\POC.Handler"),
+	[string]$dnvmVersion = 'default'
+)
+
+# start handlers
+$handlerCommands | % {  start powershell -Verb runas -argument " -noexit -command pushd $handlerPath; dnvm use $dnvmVersion; dnx $_ --QueueType $queueType"  }
+
+# start site
+start powershell -Verb runas -argument " -noexit -command pushd $sitePath; dnvm use $dnvmVersion; `$env:QueueType = '$queueType'; dnx $siteCommand"
